6acd58b424ab7efd2687b8c06769257f79d33cdd
yanxt7 <yanxt7@chinaunicom.cn>
2020-09-04 19:44:14 +0800

    fix:20200904

--- leetcode/src/main/java/arrays/AddTwoNumbers.java
+++ leetcode/src/main/java/arrays/AddTwoNumbers.java
@@ -1,6 +1,9 @@
-package main.java.arrays;
+package arrays;
 
 import main.java.bean.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * You are given two non-empty linked lists representing two non-negative
@@ -8,54 +11,82 @@ import main.java.bean.ListNode;
  * contain a single digit. Add the two numbers and return it as a linked list.
  * You may assume the two numbers do not contain any leading zero, except the
  * number 0 itself.
- * 
- * @author Dell
  *
- *         Definition for singly-linked list. public class ListNode { int val;
- *         ListNode next; ListNode(int x) { val = x; } }
- * 
- *         Example:
- * 
- *         Input: (2 -> 4 -> 3) + (5 -> 6 -> 4) Output: 7 -> 0 -> 8 Explanation:
- *         342 + 465 = 807.
+ * Definition for singly-linked list. public class ListNode { int val;ListNode next; ListNode(int x) { val = x; } }
+ * Example:
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4) Output: 7 -> 0 -> 8 Explanation: 342 + 465 = 807.
  */
 public class AddTwoNumbers {
 
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-		ListNode c1 = l1;
-		ListNode c2 = l2;
-		ListNode sentinel = new ListNode(0);
-		ListNode d = sentinel;
-		int sum = 0;
-		while (c1 != null || c2 != null) {
-			sum /= 10;
-			if (c1 != null) {
-				sum += c1.val;
-				c1 = c1.next;
-			}
-			if (c2 != null) {
-				sum += c2.val;
-				c2 = c2.next;
-			}
-			d.next = new ListNode(sum % 10);
-			d = d.next;
+		List<Integer> sumList1 = new ArrayList<Integer>();
+		List<Integer> sumList2 = new ArrayList<Integer>();
+		ListNode node1 = l1;
+		ListNode node2 = l2;
+		while (null != node1) {
+			sumList1.add(0, node1.val);
+			node1 = node1.next;
 		}
-		if (sum / 10 == 1)
-			d.next = new ListNode(1);
-		return sentinel.next;
+		while (null != node2) {
+			sumList2.add(0, node2.val);
+			node2 = node2.next;
+		}
+
+		int max = Math.max(sumList1.size(), sumList2.size());
+		List<Integer> sumList = new ArrayList<Integer>(max);
+		for (int i = 0; i < max; ++i) {
+			sumList.add(0, (i > sumList1.size() - 1 ? 0 : sumList1.get(i)) + (i > sumList2.size() - 1 ? 0 : sumList2.get(i)));
+		}
+
+		List<Integer> resList = new ArrayList<Integer>(sumList.size());
+		int over = 0;
+		for (int i = sumList.size() - 1; i > -1; --i) {
+			Integer integer = sumList.get(i);
+			integer += over;
+			if (integer > 9 && i > 0) {
+				over = integer - 9;
+				integer -= 10;
+				resList.add(0, integer);
+			} else if (integer > 9 && i == 0) {
+				resList.add(0, integer - 10);
+				resList.add(0, integer / 10);
+			} else {
+				resList.add(0, integer);
+			}
+		}
+
+		ListNode sentinel = null;
+		ListNode res = null;
+		for (int i = resList.size() - 1; i > -1; --i) {
+			if (null == sentinel) {
+				sentinel = new ListNode(resList.get(i));
+				res = sentinel;
+			} else {
+				ListNode listNode = new ListNode(resList.get(i));
+				sentinel.next = listNode;
+				sentinel = listNode;
+			}
+		}
+
+		return res;
+
 	}
 
 	public static void main(String[] args) {
 
-		ListNode l1 = new ListNode(2);
-		l1.next = new ListNode(4);
-		l1.next = new ListNode(3);
+		ListNode l10 = new ListNode(2);
+		ListNode l11 = new ListNode(8);
+		ListNode l12 = new ListNode(3);
+		l10.next = l11;
+		l11.next = l12;
 
-		ListNode l2 = new ListNode(5);
-		l2.next = new ListNode(6);
-		l2.next = new ListNode(4);
+		ListNode l20 = new ListNode(5);
+		ListNode l21 = new ListNode(6);
+		ListNode l22 = new ListNode(4);
+		l20.next = l21;
+		l21.next = l22;
 
-		System.out.println(new AddTwoNumbers().addTwoNumbers(l1, l2));
+		System.out.println(new AddTwoNumbers().addTwoNumbers(l10, l20));
 	}
 }
