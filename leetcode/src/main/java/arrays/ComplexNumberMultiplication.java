4879faff78c5bd9e3c7ec6ea269e6f8f8ffdf5ac
yanxt7 <yanxt7@chinaunicom.cn>
2020-11-20 08:59:00 +0800

    20201120

---
+++ leetcode/src/main/java/arrays/ComplexNumberMultiplication.java
@@ -0,0 +1,53 @@
+package arrays;
+
+/**
+ * Given two strings representing two complex numbers.
+ *
+ * You need to return a string representing their multiplication. Note i2 = -1 according to the definition.
+ *
+ * Example 1:
+ * Input: "1+1i", "1+1i"
+ * Output: "0+2i"
+ * Explanation: (1 + i) * (1 + i) = 1 + i2 + 2 * i = 2i, and you need convert it to the form of 0+2i.
+ * Example 2:
+ * Input: "1+-1i", "1+-1i"
+ * Output: "0+-2i"
+ * Explanation: (1 - i) * (1 - i) = 1 + i2 - 2 * i = -2i, and you need convert it to the form of 0+-2i.
+ * Note:
+ *
+ * The input strings will not have extra blank.
+ * The input strings will be given in the form of a+bi, where the integer a and b will both belong to the range of [-100, 100]. And the output should be also in this form.
+ */
+public class ComplexNumberMultiplication {
+
+	/**
+	 * Runtime: 0 ms, faster than 100.00% of Java online submissions for Complex Number Multiplication.
+	 * Memory Usage: 36.8 MB, less than 99.77% of Java online submissions for Complex Number Multiplication.
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public String complexNumberMultiply(String a, String b) {
+
+		int intA = Integer.parseInt(a.substring(0, a.indexOf('+')));
+		int intB = Integer.parseInt(a.substring(a.indexOf('+') + 1, a.length() - 1));
+		int intC = Integer.parseInt(b.substring(0, b.indexOf('+')));
+		int intD = Integer.parseInt(b.substring(b.indexOf('+') + 1, b.length() - 1));
+		return multiply(intA, intB, intC, intD);
+
+    }
+
+	private String multiply(int a, int b, int c, int d) {
+		StringBuffer sb = new StringBuffer();
+		int left = (a * c) - (b * d);
+		int right = a * d + b * c;
+		sb.append(left).append("+").append(right).append("i");
+		return sb.toString();
+	}
+
+	public static void main(String[] args) {
+		String a = "1+1i";
+		String b = "1+1i";
+		System.out.println(new ComplexNumberMultiplication().complexNumberMultiply(a, b));
+	}
+}
--- leetcode/src/main/java/arrays/TransformToChessboard.java
+++ leetcode/src/main/java/arrays/TransformToChessboard.java
@@ -43,13 +43,60 @@ package arrays;
  */
 public class TransformToChessboard {
 
+	/**
+	 * Runtime: 1 ms, faster than 98.15% of Java online submissions for Transform to Chessboard.
+	 * Memory Usage: 38.8 MB, less than 62.96% of Java online submissions for Transform to Chessboard.
+	 * @param board
+	 * @return
+	 */
 	public int movesToChessboard(int[][] board) {
 
-		return 0;
+		if (board == null || board.length == 0 || board[0].length == 0) {
+			return -1;
+		}
+		int N = board.length;
+		for (int i = 0; i < N; ++i) {
+			for (int j = 0; j < N; ++j) {
+				if ((board[0][0] ^ board[i][0] ^ board[0][j] ^ board[i][j]) == 1) {
+					return -1;
+				}
+			}
+		}
+		int rowSum = 0;
+		int colSum = 0;
+		int rowSwap = 0;
+		int colSwap = 0;
+		for (int i = 0; i < N; ++i) {
+			rowSum += board[0][i];
+			colSum += board[i][0];
+			if (board[i][0] == i % 2) {
+				++rowSwap;
+			}
+			if (board[0][i] == i % 2) {
+				++colSwap;
+			}
+		}
+		if (N / 2 > rowSum || N / 2 > (N - rowSum) ||
+				N / 2 > colSum || N / 2 > (N - colSum)) {
+			return -1;
+		}
+		if ((N & 1) == 0) {
+			rowSwap = Math.min(rowSwap, N - rowSwap);
+			colSwap = Math.min(colSwap, N - colSwap);
+		} else {
+			if ((colSwap & 1) == 1) {
+				colSwap = N - colSwap;
+			}
+			if ((rowSwap & 1) == 1) {
+				rowSwap = N - rowSwap;
+			}
+		}
+		return (rowSwap + colSwap) / 2;
+
 	}
 
 	public static void main(String[] args) {
-		int[][] board = {{0, 1, 1, 0}, {0, 1, 1, 0},{1, 0, 0, 1},{1, 0, 0, 1}};
+		int[][] board = {{0, 1, 1, 0}, {0, 1, 1, 0}, {1, 0, 0, 1}, {1, 0, 0, 1}};
 		System.out.println(new TransformToChessboard().movesToChessboard(board));
 	}
 }
